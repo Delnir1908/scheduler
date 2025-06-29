1f6b264b7d2dea56c50e284e6cc51d4bad1f5416
Jason Chen <windson7@gmail.com>
2025-06-29 14:46:23 -0700

    add CREATE mode and pass interviewers props

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,23 +1,32 @@
 import "./styles.scss";
 
 import Header from "./Header";
-
 import Empty from "./Empty";
 import Show from "./Show";
+import Form from "./Form";
+import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={props.interviewers} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={() => console.log("CONFIRM")}
-          onEdit={() => console.log("EDIT")}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,15 +2,21 @@ import { useState } from "react";
 
 export default function useVisualMode(initial){
   
-  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
-    setMode(newMode);
+  const transition = (newMode, replace = false) => {
+    if(replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
+  }
+  
+  function back() {
+    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
   }
 
-  const back = (previousMode) => {
-    setMode(previousMode);
-  }
-
-  return { mode, transition, back };
+  return { mode: history[history.length -1], transition, back };
 }
+
+
