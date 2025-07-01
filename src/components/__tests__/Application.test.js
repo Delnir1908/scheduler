de473b774404c08e973640cd6de8cbfc866f4ec2
Jason Chen <windson7@gmail.com>
2025-07-01 14:13:19 -0700

    add mock data to test Application

---
+++ src/components/__tests__/Application.test.js
@@ -0,0 +1,16 @@
+import { render, cleanup, fireEvent } from "@testing-library/react";
+
+import Application from "../Application";
+
+afterEach(cleanup);
+
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText("Monday").then(() => {
+    fireEvent.click(queryByText("Tuesday"));
+    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+  });
+});
+
