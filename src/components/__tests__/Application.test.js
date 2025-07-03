07c3cd3c583fa038f20b1339a7df3cfabb9c0961
Jason Chen <windson7@gmail.com>
2025-07-02 21:54:58 -0700

    add initial tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,16 +1,37 @@
-import { render, cleanup, fireEvent } from "@testing-library/react";
+//  We import our helper functions from the react-testing-library
+//  The render function allows us to render Components
+import { render } from "@testing-library/react";
 
-import Application from "../Application";
+// We import the component that we are testing
+import Appointment from "../Appointment";
 
-afterEach(cleanup);
 
+// A test that renders a React Component
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { queryByText, findByText } = render(<Application />);
-
-  return findByText("Monday").then(() => {
-    fireEvent.click(queryByText("Tuesday"));
-    expect(queryByText("Leopold Silvers")).toBeInTheDocument();
-  });
+it("renders without crashing", () => {
+  render(<Appointment />);
 });
 
+describe("Appointment", () => {
+  test("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  test("does something it is supposed to do", () => {
+    // ...
+  });
+
+  test("does something else it is supposed to do", () => {
+    // ...
+  });
+
+  xit("does something it is supposed to do", () => {
+    // ...
+  });
+  
+  // or if using test
+  test.skip("does something it is supposed to do", () => {
+    // ...
+  });
+  
+});
