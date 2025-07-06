53e1dccf8f7e606c7d75fefe760a6c019dc3a832
Jason Chen <windson7@gmail.com>
2025-07-05 19:28:13 -0700

    scope container with queries

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,9 +1,17 @@
 //  We import our helper functions from the react-testing-library
 //  The render function allows us to render Components
-import { render } from "@testing-library/react";
+import {
+  getAllByTestId,
+  queryByText,
+  render,
+  getyByText,
+  findByText,
+  prettyDOM
+} from "@testing-library/react";
 
 // We import the component that we are testing
 import Appointment from "../Appointment";
+import Application from "../Application";
 
 
 // A test that renders a React Component
@@ -35,3 +43,26 @@ describe("Appointment", () => {
   });
   
 });
+
+describe("Application", () => {
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+
+    const appointment = getAllByTestId(container, "appointment")[0];
+    console.log(prettyDOM(appointment));
+
+    ireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, "Save"));
+  });
+
+  
+});
+
+
+
